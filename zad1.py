a2893deac3e2e66391a84276b7e062b472fce417
marcion7 <58213967+marcion7@users.noreply.github.com>
2023-10-12 15:45:53 +0200

    Update zad1.py

--- zad1.py
+++ zad1.py
@@ -1,30 +1,29 @@
 import pandas as pd
 
-class wczytaj_baze_probek_z_tekstem:
+class WczytajBazeProbekZTekstem:
     def __init__(self, nazwa_pliku_z_wartosciami, nazwa_pliku_z_opisem_atr):
         self.nazwa_pliku_z_wartosciami = nazwa_pliku_z_wartosciami
         self.nazwa_pliku_z_opisem_atr = nazwa_pliku_z_opisem_atr
         
-    def probki(self):
+    def Probki(self):
         try:
             probki = pd.read_csv(self.nazwa_pliku_z_wartosciami, sep="\s+", header=None)
-            probki = probki.T
             return probki
         except:
             print("Każda pojedyncza linia (próbka) musi zawierać taką samą liczbę wyrazów (wartości kolejnych atrybutów, deskryptorów)")
     
-    def nazwy_atr(self):
+    def NazwyAtr(self):
         atr = pd.read_csv(self.nazwa_pliku_z_opisem_atr, sep="\s+", header=None)
         return atr[0]
     
-    def czy_atr_symb(self, wiersz):
+    def CzyAtrSymb(self, wiersz):
         atr = pd.read_csv(self.nazwa_pliku_z_opisem_atr, sep="\s+", header=None)
         if atr[1][wiersz] == 's':
             return True
         else:
             return False
 
-test = wczytaj_baze_probek_z_tekstem("war.txt", "atr.txt")
-print(test.probki()[0][2])
-print(test.czy_atr_symb(4))
-print(test.nazwy_atr()[4])
+test = WczytajBazeProbekZTekstem("war.txt", "atr.txt")
+print(test.Probki()[0][2])
+print(test.CzyAtrSymb(4))
+print(test.NazwyAtr()[4])
