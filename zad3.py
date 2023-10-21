313b9c7fc0d8c16944464348b685dddbef3a06f3
marcion7 <58213967+marcion7@users.noreply.github.com>
2023-10-21 14:45:17 +0200

    Update zad3.py

--- zad3.py
+++ zad3.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 from zad1 import WczytajBazeProbekZTekstem
 
 def EucDist(p1, p2):
     return np.sqrt(pow(p2.x-p1.x, 2)+pow(p2.y-p1.y, 2))
 
-def KMeans(data, iters, n):
+def KMeans(data, n, iters):
     V = data.sample(n)
     V = V.reset_index()
     data['Us'] = None
@@ -24,12 +25,31 @@ def KMeans(data, iters, n):
             else:
                 V.loc[j, 'x'] = Xgr['x'].mean()
                 V.loc[j, 'y'] = Xgr['y'].mean()
-    return V
+        
+    plt.title(f'Algorytm k-średnich \n Liczba iteracji: {iters}')
+    kMeansPlot(V, data)
 
-colors = ['r', 'b', 'g', 'y']
+def kMeansPlot(V, data):
+    color = cm.viridis_r(np.linspace(0, 1, len(V.index)))
+    for n in range(len(V.index)):
+        part = data.loc[data['Us'] == n]
+        plt.scatter(
+            part['x'],
+            part['y'],
+            c=color[n]
+        )
+        plt.scatter(
+            V.loc[n, 'x'],
+            V.loc[n, 'y'],
+            c=color[n],
+            marker='s',
+            edgecolors='#000000',
+            label=(f"group {n}")
+        )
+    plt.xlabel('atrybut 1')
+    plt.ylabel('atrybut 2')
+    plt.legend()
+    plt.show()
 
 test = WczytajBazeProbekZTekstem("spirala.txt", "spirala-type.txt")
-print(KMeans(test.Probki(), 3, 4))
-
-
-
+KMeans(test.Probki(), 4, 11)
