a45a2a8e732e9bb0b8e5bcd3bae2749ba7f486ce
marcion7 <58213967+marcion7@users.noreply.github.com>
2023-10-24 19:44:29 +0200

    Update zad3.py

--- zad3.py
+++ zad3.py
@@ -6,50 +6,48 @@ from zad1 import WczytajBazeProbekZTekstem
 def EucDist(p1, p2):
     return np.sqrt(pow(p2.x-p1.x, 2)+pow(p2.y-p1.y, 2))
 
-def KMeans(data, n, iters):
+def KMeans(data, iters, n):
     V = data.sample(n)
     V = V.reset_index()
     data['Us'] = None
     for _ in range(iters):
         for s in range(len(data.index)):
             distance = np.inf
-            for x in range(len(V.index)):
-                if distance > EucDist(V.iloc[x], data.iloc[s]):
-                    distance = EucDist(V.iloc[x], data.iloc[s])
-                    data.at[s, 'Us'] = x
+            for j in range(len(V.index)):
+                if EucDist(V.iloc[j], data.iloc[s]) < distance:
+                    distance = EucDist(V.iloc[j], data.iloc[s])
+                    data.at[s, 'Us'] = j
 
-        for j in range(len(V.index)):
-            Xgr = data.loc[data['Us'] == j]
-            if Xgr is None:
-                continue
+        for i in range(len(V.index)):
+            Xgr = data.loc[data['Us'] == i]
+            if Xgr.size == 0:
+                break
             else:
-                V.loc[j, 'x'] = Xgr['x'].mean()
-                V.loc[j, 'y'] = Xgr['y'].mean()
-        
-    plt.title(f'Algorytm k-średnich \n Liczba iteracji: {iters}')
-    kMeansPlot(V, data)
+                V.loc[i, 'x'] = Xgr['x'].mean()
+                V.loc[i, 'y'] = Xgr['y'].mean()
+    plt.title(f'Algorytm k-średnich\nLiczba iteracji: {iters}')
+    KMeans_plot(V, data)
 
-def kMeansPlot(V, data):
+def KMeans_plot(V, data):
     color = cm.viridis_r(np.linspace(0, 1, len(V.index)))
     for n in range(len(V.index)):
         part = data.loc[data['Us'] == n]
         plt.scatter(
             part['x'],
             part['y'],
-            c=color[n]
-        )
+            color=color[n])
         plt.scatter(
             V.loc[n, 'x'],
             V.loc[n, 'y'],
-            c=color[n],
-            marker='s',
+            color=color[n],
+            marker='^',
             edgecolors='#000000',
-            label=(f"group {n}")
-        )
+            label=f"group {n}")
     plt.xlabel('atrybut 1')
     plt.ylabel('atrybut 2')
-    plt.legend()
+    plt.legend(loc="upper right")
     plt.show()
 
+
 test = WczytajBazeProbekZTekstem("spirala.txt", "spirala-type.txt")
-KMeans(test.Probki(), 4, 11)
+KMeans(test.Probki(), 11, 4)
