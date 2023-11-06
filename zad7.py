d735fe6cfdf75068d536c0fcdb4cffec1946d533
marcion7 <58213967+marcion7@users.noreply.github.com>
2023-11-06 19:27:48 +0100

    Update zad7.py

--- zad7.py
+++ zad7.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 baza_wzor = [
     np.array([[1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1]]),
@@ -27,9 +28,7 @@ class Siec_hopfielda:
             for j in range(self.n):
                 if i == j:
                     continue
-                self.weights_table[i][j] += wyjscie[0][i] * wyjscie[0][j]
-            self.weights_table[i] /= self.n
-        return self.weights_table
+                self.weights_table[i][j] += (wyjscie[0][i] * wyjscie[0][j]) / self.n
     
     def rozpoznaj_obraz(self, obraz):
         wyjscie = obraz.reshape(1, len(obraz[0]) * len(obraz[1]))
@@ -40,15 +39,24 @@ class Siec_hopfielda:
                     continue
                 suma += wyjscie[0][j] * self.weights_table[i][j]
             wyjscie[0][i] = 1 if suma >= 0 else -1
-        if np.array_equal(wyjscie, obraz):
-            return False
-        return obraz
+        obraz_skorygowany = wyjscie.reshape(len(obraz[0]), len(obraz[1]))
+        obraz_negatyw = obraz
+        for x in range(len(obraz[0])):
+            for y in range(len(obraz[1])):
+                obraz_negatyw[x, y] = 1 if obraz_negatyw[x, y] == -1 else -1
+        if np.array_equal(obraz_skorygowany, obraz) or np.array_equal(obraz_skorygowany, obraz_negatyw):
+            return obraz_skorygowany
+        return self.rozpoznaj_obraz(obraz_skorygowany)
     
 hopfield = Siec_hopfielda(5,5)
 for wzor in baza_wzor:
     hopfield.naucz_obraz(wzor)
 
 for i in range(len(baza_test)):
-    print(f'Znak testowy {i+1}')
-    print(hopfield.rozpoznaj_obraz(baza_test[i]))
-    print('-------------------------------------------------------')
+    plt.subplot(1, 2, 1)
+    plt.imshow(baza_test[i], interpolation='nearest', cmap='Greys')
+    plt.title(f'Znak testowy {i+1}')
+    plt.subplot(1, 2, 2)
+    plt.imshow(hopfield.rozpoznaj_obraz(baza_test[i]), interpolation='nearest', cmap='Greys')
+    plt.title(f'Po korekcji')
+    plt.show()
