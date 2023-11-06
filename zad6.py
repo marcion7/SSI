c9ae4edd6c4e592ab65c751f833af9a997221b41
marcion7 <58213967+marcion7@users.noreply.github.com>
2023-11-06 19:41:24 +0100

    Update zad6.py

--- zad6.py
+++ zad6.py
@@ -1,62 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def euc_distance2d(p1, p2):
+    return np.sqrt(pow(p2[0]-p1[0], 2)+pow(p2[1]-p1[1], 2))
+
 baza_wzor = [
-    np.array([[1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1]]),
-    np.array([[1, -1, -1, -1, 1], [-1, 1, -1, 1, -1], [-1, -1, 1, -1, -1], [-1, 1, -1, 1, -1], [1, -1, -1, -1, 1]]),
-    np.array([[-1, -1, 1, -1, -1], [-1, -1, 1, -1, -1], [1, 1, 1, 1, 1], [-1, -1, 1, -1, -1], [-1, -1, 1, -1, -1]]),
+    np.array([[0, 0, 0, 1], [0, 0, 1, 1], [0, 1, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1]]),
+    np.array([[0, 1, 1, 1], [1, 0, 0, 1], [0, 0, 1, 0], [0, 1, 0, 0], [1, 1, 1, 1]]),
+    np.array([[1, 1, 1, 0], [0, 0, 0, 1], [1, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 0]]),
 ]
 
 baza_test = [
-    np.array([[-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1], [-1, 1, -1, -1, -1]]),
-    np.array([[1, 1, -1, -1, 1], [-1, 1, -1, 1, -1], [-1, 1, 1, 1, -1], [-1, 1, -1, 1, -1], [1, 1, -1, -1, 1]]),
-    np.array([[-1, -1, -1, -1, -1], [-1, -1, 1, -1, -1], [1, 1, 1, 1, 1], [-1, -1, -1, -1, -1], [-1, -1, 1, -1, -1]]),
-    np.array([[-1, 1, 1, 1, 1], [1, -1, 1, 1, 1], [1, -1, 1, 1, 1], [1, -1, 1, 1, 1], [1, -1, 1, 1, 1]]),
+    np.array([[0, 0, 0, 0], [0, 0, 1, 1], [0, 1, 1, 1], [0, 0, 0, 1], [0, 0, 0, 1]]),
+    np.array([[1, 1, 1, 1], [0, 0, 0, 1], [1, 1, 1, 1], [0, 0, 1, 1], [1, 1, 1, 1]]),
+    np.array([[1, 1, 1, 1], [0, 0, 0, 1], [0, 0, 1, 0], [1, 1, 0, 0], [1, 1, 1, 1]]),
 ]
 
+def miara_nieprawdopodobienstwa(BA, BB):
+    miara = 0
+    for pay in range(len(BA)):
+        for pax in range(len(BA[pay])):
+            if BA[pay, pax] == 1:
+                odl_min = np.inf
+                for pby in range(len(BB)):
+                    for pbx in range(len(BB[pby])):
+                        if BB[pby, pbx] == 1:
+                            odl_akt = euc_distance2d((pay, pax),(pby, pbx))
+                            odl_min = min(odl_min, odl_akt)
+                miara += odl_min
+    return miara
 
-class Siec_hopfielda:
-    def __init__(self, h, w):
-        self.h = h
-        self.w = w
-        self.n = h * w
-        self.weights_table = np.zeros((self.n, self.n))
-    
-    def naucz_obraz(self, wzor):
-        wyjscie = wzor.reshape(1, len(wzor[0]) * len(wzor[1]))
-        for i in range(self.n):
-            for j in range(self.n):
-                if i == j:
-                    continue
-                self.weights_table[i][j] += (wyjscie[0][i] * wyjscie[0][j]) / self.n
-    
-    def rozpoznaj_obraz(self, obraz):
-        wyjscie = obraz.reshape(1, len(obraz[0]) * len(obraz[1]))
-        for i in range(self.n):
-            suma = 0
-            for j in range(self.n):
-                if i == j:
-                    continue
-                suma += wyjscie[0][j] * self.weights_table[i][j]
-            wyjscie[0][i] = 1 if suma >= 0 else -1
-        obraz_skorygowany = wyjscie.reshape(len(obraz[0]), len(obraz[1]))
-        obraz_negatyw = np.copy(obraz)
-        for x in range(len(obraz_negatyw[0])):
-            for y in range(len(obraz_negatyw[1])):
-                obraz_negatyw[x, y] = 1 if obraz_negatyw[x, y] == -1 else -1
-        if np.array_equal(obraz_skorygowany, obraz) or np.array_equal(obraz_skorygowany, obraz_negatyw):
-            return obraz_skorygowany
-        return self.rozpoznaj_obraz(obraz_skorygowany)
-    
-hopfield = Siec_hopfielda(5,5)
-for wzor in baza_wzor:
-    hopfield.naucz_obraz(wzor)
+def miara_podobienstwa_obustronnego(BA, BB):
+    wynik = miara_nieprawdopodobienstwa(BA, BB) + miara_nieprawdopodobienstwa(BB, BA)
+    return -wynik
+
+def zachlanne_dopasowanie(bitmap_test, bitmap_wzor):
+    miara = -np.inf
+    index = 0
+    for i in range(len(bitmap_wzor)):
+        miara_podob_obustronnego = miara_podobienstwa_obustronnego(bitmap_test, bitmap_wzor[i])
+        if miara_podob_obustronnego > miara:
+            miara = miara_podob_obustronnego
+            index = i
+    return bitmap_test, baza_wzor[index]
 
 for i in range(len(baza_test)):
+    bitmap_test, bitmap_wzor = zachlanne_dopasowanie(baza_test[i], baza_wzor)
+    print(f'Dla bitmapy testowej {i+1}:\n{bitmap_test}\nNajbardziej podobna jest bitmapa wzorcowa:\n{bitmap_wzor}')
     plt.subplot(1, 2, 1)
-    plt.imshow(baza_test[i], interpolation='nearest', cmap='Greys')
-    plt.title(f'Znak testowy {i+1}')
+    plt.imshow(bitmap_test, interpolation='nearest', cmap='Greys')
+    plt.title(f'Bitmapa testowa {i+1}')
     plt.subplot(1, 2, 2)
-    plt.imshow(hopfield.rozpoznaj_obraz(baza_test[i]), interpolation='nearest', cmap='Greys')
-    plt.title(f'Po korekcji')
+    plt.imshow(bitmap_wzor, interpolation='nearest', cmap='Greys')
+    plt.title(f'Najbardziej podobny wzór')
     plt.show()
